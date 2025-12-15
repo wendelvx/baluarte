ba98bc9c488a1d93046c9c7cd5f0948725ca3b29
wendel <capauzumaki@gmail.com>
2025-12-15 01:30:21 -0300

    Ajustndo cors

--- web/src/sanity.js
+++ web/src/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: 'd9lyzv56', 
+  projectId: 'hv051tjr', 
   dataset: 'production',
   useCdn: true, 
   apiVersion: '2023-01-01',
